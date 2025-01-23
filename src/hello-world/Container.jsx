305bb57121341e64174caa6d2939e5d074c2d2af
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2025-01-23 20:51:11 +0700

    ✨ feat: nested component

---
+++ src/hello-world/Container.jsx
@@ -0,0 +1,11 @@
+export default function Container({ children }) {
+  return (
+    <div>
+      <h1>Muhammad Dwi Susanto</h1>
+      {children}
+      <footer>
+        <p>Copyright &copy; 2022</p>
+      </footer>
+    </div>
+  );
+}
