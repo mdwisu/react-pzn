de4d929ab70f89eb88cd15e53900c1f38839fb6e
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2025-01-23 20:57:48 +0700

    ✨ feat: style

--- src/hello-world/HelloWorld.jsx
+++ src/hello-world/HelloWorld.jsx
@@ -1,24 +1,21 @@
+import "./HelloWorld.css";
+
 export default function HelloWorld() {
   const props = {
     text: "Ups, lupa kasi text",
   };
   return (
     <>
-      <h1>hello world</h1>
       <HeaderHelloWorld {...props} />
       <ParagraphHelloWorld />
     </>
   );
 }
 function HeaderHelloWorld({ text = "Ups, lupa kasi text" }) {
-  return (
-    <h1 style={{ color: "red", backgroundColor: "yellow" }}>
-      {text.toUpperCase()}
-    </h1>
-  );
+  return <h1 className="title">{text.toUpperCase()}</h1>;
 }
 
 function ParagraphHelloWorld() {
   const text = "Selamat belajar reactjs";
-  return <p>{text.toUpperCase()}</p>;
+  return <p className="content">{text.toUpperCase()}</p>;
 }
