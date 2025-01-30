4b714c08afc1539232ef38d05672c6d02954ce8b
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2025-01-30 21:27:18 +0700

    ✨ feat: ref

--- src/button/AlertButton.jsx
+++ src/button/AlertButton.jsx
@@ -1,7 +1,10 @@
+import { useRef } from "react";
+
 export default function AlertButton({ text, message }) {
+  const counter = useRef(0);
   function handleClick(e) {
     console.info(e.target);
-    alert(message);
+    alert(`message: ${message} counter: ${counter.current++}`);
   }
 
   return (
