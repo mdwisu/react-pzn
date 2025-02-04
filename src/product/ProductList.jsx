b391779246c010cdb800c5ede6f3fe5f0d1e3ac1
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2025-02-04 10:31:54 +0700

    ✨ feat: useEffect

---
+++ src/product/ProductList.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useRef, useState } from "react";
+import Product from "./Product";
+
+export default function ProductList() {
+  const [product, setProduct] = useState([]);
+  const loaded = useRef(false);
+
+  useEffect(() => {
+    if (loaded.current === false) {
+      fetch("/product.json")
+        .then((res) => res.json())
+        .then((data) => setProduct(data))
+        .then(() => (loaded.current = true))
+        .catch((err) => console.log(err));
+    }
+  });
+  
+  return (
+    <>
+      <h1>Product List</h1>
+      {product.map((item) => (
+        <Product key={item.id} product={item} />
+      ))}
+    </>
+  );
+}
