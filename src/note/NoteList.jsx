24d29f701a2fa48cf9120656ecbdbd031e9e9276
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2025-02-05 23:06:37 +0700

    ✨ feat: useMemo

--- src/note/NoteList.jsx
+++ src/note/NoteList.jsx
@@ -1,13 +1,29 @@
-import { useContext } from "react";
+import { useContext, useMemo, useRef, useState } from "react";
 import Note from "./Note";
 import { NotesContext } from "./NoteContect";
 
 export default function NoteList() {
   const notes = useContext(NotesContext);
+  const [search, setSearch] = useState("");
+  const searchInput = useRef(null);
+
+  const filteredNotes = useMemo(() => {
+    return notes.filter((note) => {
+      console.info('Filtering notes')
+      return note.text.toLowerCase().includes(search.toLowerCase());
+    });
+  }, [search, notes]);
+
+  function handleSearch() {
+    console.info("search")
+    setSearch(searchInput.current.value);
+  }
   return (
     <div>
+      <input ref={searchInput} placeholder="search" />
+      <button onClick={handleSearch}>Search</button>
       <ul>
-        {notes.map((note) => (
+        {filteredNotes.map((note) => (
           <li key={note.id}>
             <Note note={note} />
           </li>
